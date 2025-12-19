72747eb4be87a52d8491a9ecaf05d703cdb9c5a2
egorcha174 <83664062+egorcha174@users.noreply.github.com>
2025-12-19 11:43:30 +0500

    Create UiEntityCard.svelte

---
+++ src/components/UiEntityCard.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import type { UiEntity } from '$lib/models';
+  import BinaryCard from './cards/BinaryCard.svelte';
+  import SensorCard from './cards/SensorCard.svelte';
+  import InfoCard from './cards/InfoCard.svelte';
+
+  export let entity: UiEntity;
+</script>
+
+<!-- 
+  Диспетчер карточек: выбирает правильный компонент на основе типа сущности (kind)
+-->
+{#if entity.kind === 'binary'}
+  <BinaryCard {entity} />
+{:else if entity.kind === 'sensor'}
+  <SensorCard {entity} />
+{:else}
+  <InfoCard {entity} />
+{/if}
+
+<style>
+  /* Стили будут в конкретных компонентах карточек */
+</style>
