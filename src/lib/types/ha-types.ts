fb2e8b62c66693d74a97686e0aca0ce57aa8afa5
egorcha174 <83664062+egorcha174@users.noreply.github.com>
2025-12-19 11:37:48 +0500

    Create ha-types.ts

---
+++ src/lib/types/ha-types.ts
@@ -0,0 +1,5 @@
+// Re-export Home Assistant types with convenient aliases
+export type { HAEntity as Entity } from './ha';
+export type { HAState as HaState } from './ha';
+export type { HAServerConfig as ServerConfig } from './ha';
+export type { HAConnectionStatus as ConnectionStatus } from './ha';
