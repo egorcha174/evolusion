4bf9860b719b5b154c1760574e7ac2a24556de26
egorcha174 <83664062+egorcha174@users.noreply.github.com>
2025-12-19 11:36:11 +0500

    Create index.ts

---
+++ src/lib/models/index.ts
@@ -0,0 +1,13 @@
+// Выствляем все необходимые типы и функции
+
+// Модели и типы
+export * from './ui-model';
+
+// Логика определения kind
+export * from './entity-kind';
+
+// Логика действий
+export * from './entity-actions';
+
+// Маппинг HA states в UiEntity
+export * from './ui-mapper';
